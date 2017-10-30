5ad5ee98745fac9dcadca08c65374b9586ee0f67
MinhTri <minh_tri585@yahoo.com>
2017-10-30 22:45:02 +0700

    chapter 5

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>Hello TEDU Online Angular 2!</h1>
+  <my-tutorial></my-tutorial>
+  `
+})
+export class AppComponent { }
---
+++ app/tutorial.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-tutorial',
+    template: '<h2>tutorial component</h2>'
+})
+export class TutorialComponent{
+
+}
