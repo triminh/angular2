dc186454747eba120e9ad0a18966de1e3f3f8474
MinhTri <minh_tri585@yahoo.com>
2017-11-28 18:55:44 +0700

    Services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
-  styleUrls:['app/app.component.css']
 })
 export class AppComponent {
-  public cities = [
-    { Id: 1, Name: "HCM" }, 
-    { Id: 2, Name: "CT" }, 
-    ];
-  onSubmit(value:any) {
-  console.log(value);
+
 }
